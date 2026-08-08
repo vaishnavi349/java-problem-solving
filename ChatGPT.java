2fe948676501f9c1eb98f4aabe4b241acb3d1616
vaishnavi349 <vankalasvaishnavi9@gmail.com>
2026-08-08 22:30:53 +0530

    Add files via upload

---
+++ ChatGPT.java
@@ -0,0 +1,11 @@
+class ChatGPT{
+    public static void main(String[] args)
+    {
+	String str="ChatGPT";
+     	for(int i=0;i<str.length();i++)
+	{
+	 char ch=str.charAt(i);
+	 System.out.println(i+" "+ch);
+	}
+      }
+}
